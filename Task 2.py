e922209471b61d696a1cd0cdfe54e64d6d8cfc37
Mackachoo <Mackachooo@gmail.com>
2020-11-24 00:58:41 +0000

    Added functions and main program
    New Functions: scores and selection

--- Task 2.py
+++ Task 2.py
@@ -1,16 +1,13 @@
 import numpy as np
 import random as r
 
-cRate = 0.5
-mRate = 0.01
-
+### Functions ----------------------------------------------------------------------------
 
 def createInitials(N):
     initials = []
     for i in range(N):
         initials.append(np.random.randint(1,8,(r.randint(1,6))))
-    return np.array(initials)
-
+    return np.array(initials, dtype=object)
 
 def mutate(nw, mR):
     for i in range(len(nw)):
@@ -37,8 +34,41 @@ def crossover(nw1, nw2, cR):
                 nw1[i], nw2[i] = nw2[i], nw1[i]
     return nw1[nw1 != 0], nw2[nw2 != 0]
 
-print(crossover(np.array([1,2,3]), np.array([9,8,7,6]), 0.3))
+def scores(nws):
+    nwSc = []
+    for nw in nws:
+        nwSc.append(r.randint(0,100))
+    nwSc = np.array(nwSc)
+    nwSc = np.cumsum(nwSc/np.sum(nwSc))
+    return np.column_stack((nws,nwSc))
 
 
+def selection(scored, pairs):
+    selected = []
+    print(scored)
+    for i in range(2*pairs):
+        rInt = r.random()
+        for obj in scored:
+            if obj[1] > rInt:
+                selected.append(obj[0])
+                break
+    return np.array(selected, dtype=object)
 
-#np.random.randint(1,8,(1,r.randint(1,6)))
+### Constants ----------------------------------------------------------------------------
+
+cRate = 0.5
+mRate = 0.01
+Iterations = 10
+NumInitials = 100
+
+### Program ------------------------------------------------------------------------------
+
+networkSet = createInitials(NumInitials)
+for i in range(Iterations):
+    selected = selection(scores(networkSet))
+    networkSet = []
+    for pair in range(len(selected)//2):
+        nwP1, nwP2 = crossover(selected[2*pair], selected[2*pair+1], cRate)
+        nwP1 = mutate(nwP1, mRate)
+        nwP2 = mutate(nwP2, mRate)
+
